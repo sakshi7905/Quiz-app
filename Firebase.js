77cbe3bdf6eb1f1a9ba589d71b95b025b0420033
sakshi7905 <86019100+sakshi7905@users.noreply.github.com>
2021-08-06 20:36:44 +0530

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBhTchPTKXMRyg7ORy8uPIPmMRXDz74iEc",
+  authDomain: "quiz-app-26c40.firebaseapp.com",
+  projectId: "quiz-app-26c40",
+  storageBucket: "quiz-app-26c40.appspot.com",
+  messagingSenderId: "498791812421",
+  appId: "1:498791812421:web:9d91dcb03ba2e3f36dc22c",
+  measurementId: "G-MDZH6QK48Z",
+};
